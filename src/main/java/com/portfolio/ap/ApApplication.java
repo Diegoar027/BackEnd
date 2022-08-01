b7e9a8fcd879cd65923368217b3e14a834b285e0
Diegoar027 <ariasdiego027@gmail.com>
2022-07-31 21:16:16 -0300

    prueba cors

--- src/main/java/com/portfolio/ap/ApApplication.java
+++ src/main/java/com/portfolio/ap/ApApplication.java
@@ -2,6 +2,9 @@ package com.portfolio.ap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApApplication {
@@ -9,5 +12,19 @@ public class ApApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApApplication.class, args);
 	}
-
+        
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://approyectofront.web.app").allowedMethods("GET", "POST", "PUT", "PATCH",
+        "DELETE", "OPTIONS");
 }
+};
+}
+}
+
+
+        
+
